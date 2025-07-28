94c9b104a243675c4ee27712179e8e13dccf3b5c
Vineet Singh <singhvineet00111@gmail.com>
2025-07-28 13:22:10 +0530

    Updated with  new logic

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://travel-website-85xf.onrender.com",
   credentials: true,
 };
 
